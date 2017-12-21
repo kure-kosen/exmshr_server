81fe656b037f60eb77971695cc43ed1874872285
taira120 <taira1999120@gmail.com>
2017-12-21 13:48:31 +0900

    install ams

--- test/controllers/exams_controller_test.rb
+++ test/controllers/exams_controller_test.rb
@@ -5,40 +5,40 @@ class ExamsControllerTest < ActionDispatch::IntegrationTest
     @exam = exams(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get exams_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_exam_url
     assert_response :success
   end
 
-  test "should create exam" do
+  test 'should create exam' do
     assert_difference('Exam.count') do
-      post exams_url, params: { exam: {  } }
+      post exams_url, params: { exam: {} }
     end
 
     assert_redirected_to exam_url(Exam.last)
   end
 
-  test "should show exam" do
+  test 'should show exam' do
     get exam_url(@exam)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_exam_url(@exam)
     assert_response :success
   end
 
-  test "should update exam" do
-    patch exam_url(@exam), params: { exam: {  } }
+  test 'should update exam' do
+    patch exam_url(@exam), params: { exam: {} }
     assert_redirected_to exam_url(@exam)
   end
 
-  test "should destroy exam" do
+  test 'should destroy exam' do
     assert_difference('Exam.count', -1) do
       delete exam_url(@exam)
     end
